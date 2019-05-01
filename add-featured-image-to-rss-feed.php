b1916b9116c27f56b583beefc0ab868ea5ba42cb
Corey Salzano <corey.salzano@gmail.com>
2019-05-01 00:44:10 -0400

    Wraps the thumbnail size in a filter. Changes the plugin header comment with updated URLs and GPL2 licensing.

--- add-featured-image-to-rss-feed.php
+++ add-featured-image-to-rss-feed.php
@@ -1,22 +1,23 @@
 <?php
-/* 
-Plugin Name: Add featured image to RSS feed
-Plugin URI: http://www.tacticaltechnique.com/wordpress/
-Description: Adds the featured image attached to your posts to the RSS feed.
-Author: Corey Salzano
-Version: 1.0
-Author URI: http://twitter.com/salzano
-*/
+defined( 'ABSPATH' ) or exit;
+/**
+ * Plugin Name: Add Featured Image to RSS Feed
+ * Plugin URI: https://github.com/csalzano/add-featured-image-to-rss-feed
+ * Description: Adds the featured image attached to your posts to the RSS feed.
+ * Version: 1.1.0
+ * Author: Corey Salzano
+ * Author URI: https://coreysalzano.com/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-	function add_featured_image_to_feed($content) {
-		global $post;
-		if ( has_post_thumbnail( $post->ID ) ){
-			$content = '' . get_the_post_thumbnail( $post->ID, 'large' ) . '' . $content;
-		}
-		return $content;
+function salzano_add_featured_image_to_feed( $content ) {
+	global $post;
+	if ( isset( $post->ID ) && has_post_thumbnail( $post->ID ) ){
+		return get_the_post_thumbnail( $post->ID, apply_filters( 'rss_featured_image_thumbnail_size', 'large' ) ) . $content;
 	}
+	return $content;
+}
 
-	add_filter('the_excerpt_rss', 'add_featured_image_to_feed', 1000, 1);
-	add_filter('the_content_feed', 'add_featured_image_to_feed', 1000, 1);
-
-?>
+add_filter( 'the_excerpt_rss', 'salzano_add_featured_image_to_feed', 1000, 1 );
+add_filter( 'the_content_feed', 'salzano_add_featured_image_to_feed', 1000, 1 );

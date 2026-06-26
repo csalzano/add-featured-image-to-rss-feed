bea458f73b0e81fff43cc852debb37b302ca5785
Corey Salzano <938411+csalzano@users.noreply.github.com>
2026-06-26 12:24:10 -0400

    Bumps tested up to version to 7.0. Bumps version to 1.1.5.

--- add-featured-image-to-rss-feed.php
+++ add-featured-image-to-rss-feed.php
@@ -1,15 +1,16 @@
 <?php
-defined( 'ABSPATH' ) or exit;
 /**
  * Plugin Name: Add Featured Image to RSS Feed
  * Plugin URI: https://github.com/csalzano/add-featured-image-to-rss-feed
  * Description: Adds the featured image attached to your posts to the RSS feed.
- * Version: 1.1.4
+ * Version: 1.1.5
  * Author: Corey Salzano
  * Author URI: https://coreysalzano.com/
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
+
+defined( 'ABSPATH' ) || exit;
 
 function salzano_add_featured_image_to_feed( $content ) {
 	global $post;
